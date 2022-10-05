ee3a77c90c0fa05d01509bca09e454b3cca33250
吴俊城 <wujunchengman@163.com>
2022-10-05 16:56:31 +0800

    同步笔记，准备回成都

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,17 @@
+import { defaultTheme, defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '考试准备中',
+  description: '对考试内容的复习准备',
+  theme: defaultTheme({
+    repoLabel: "我的博客",
+    repo: "https://cnblogs.com/wujuncheng",
+    navbar: [
+        {
+            text: '数据库系统原理',
+            link: '/DataBase/NormalForm.md'
+        }
+    ]
+  })
+})
